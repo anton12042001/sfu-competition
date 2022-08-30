08374bb2090fd7dd19f4865857c3f799010b7a5e
Anton <98453976+anton12042001@users.noreply.github.com>
2022-08-30 10:02:35 +0700

    Sending an email to confirm mail

--- src/components/Authorization/Register/RegisterContainer.jsx
+++ src/components/Authorization/Register/RegisterContainer.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Register from "./Register";
-import {createUserWithEmailAndPassword, getAuth} from "firebase/auth";
+import {createUserWithEmailAndPassword, getAuth, sendEmailVerification} from "firebase/auth";
 import {setUser} from "../../../reduxTollkit/slices/userSlice";
 import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router-dom";
+import cl from './Register.module.css'
 
 const RegisterContainer = () => {
+    const [showMessagesEmailVerf, setShowMessagesEmailVerf] = useState(false)
+    const [emailVerf, setEmailVerf] = useState(false)
 
     const dispatch = useDispatch()
     const auth = getAuth();
     const navigate = useNavigate()
+
+
+    useEffect(() => {
+        let currentUser = auth.currentUser
+        setEmailVerf(currentUser.emailVerified)
+        if(emailVerf === true){
+            navigate('/content')
+        }
+    },[] )
+
 
     const handleRegister = (email, password) => {
         createUserWithEmailAndPassword(auth, email, password)
@@ -19,7 +32,16 @@ const RegisterContainer = () => {
                     id: user.uid,
                     token: user.accessToken,
                 }))
-                navigate('/content')
+                let currentUser = auth.currentUser
+
+                sendEmailVerification(auth.currentUser)
+                    .then(() => {
+                        setShowMessagesEmailVerf(true)
+                    })
+
+                // navigate('/content')
+
+
             })
             .catch(console.error)
     }
@@ -27,7 +49,8 @@ const RegisterContainer = () => {
 
     return (
         <div>
-            <Register handleRegister={handleRegister}/>
+            <div className={cl.register} ><Register handleRegister={handleRegister}/></div>
+            {showMessagesEmailVerf && <div className={cl.sendVerf}>Письмо для подтверждения email отправлено на указанную вами почту.</div>}
         </div>
     );
 };
